1deef22530bff7169bb8cdd5a934f53bc35663f4
Zaira Aldama <2203068411@correo.ler.uam.mx>
2025-12-11 01:12:16 -0600

    initial commit

---
+++ src/main/java/com/example/carritodorado/CarritoDoradoApplication.java
@@ -0,0 +1,13 @@
+package com.example.carritodorado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoDoradoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarritoDoradoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carritodorado/CarritoDoradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carritodorado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoDoradoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
